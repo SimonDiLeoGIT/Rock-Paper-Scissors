82801e40ff6e557605fb697a0939d4c0468e4ff5
SimonDiLeoGIT <saimondiley@gmail.com>
2023-12-27 13:13:15 -0300

    Mejora estilos original game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { BonusGame } from "./pages/BonusGame";
 function App() {
 
   return (
-    <main class="w-screen min-h-screen m-auto text-center font-sans bg-gradient-to-b from-radial-gradient-from to-radial-gradient-to text-white p-8">
+    <main class=" w-screen h-screen m-auto text-center font-sans bg-gradient-to-b from-radial-gradient-from to-radial-gradient-to text-white p-8">
       <h1 className="text-4xl text-white font-semibold border-4 border-header-outline inline-block p-8 rounded-xl"> Rock, Paper, Scissors Game </h1>
       <section className="game-options">
         <h2 className="text-3xl p-4 mt-12 font-semibold"> Game mode </h2>
--- client/src/pages/OriginalGame.jsx
+++ client/src/pages/OriginalGame.jsx
@@ -4,28 +4,30 @@ import rock_icon from "./../assets/images/icon-rock.svg"
 import logo from "./../assets/images/logo.svg"
 import rules from "./../assets/images/image-rules.svg"
 import { useState } from "react";
+import bgImage from "./../assets/images/bg-triangle.svg"
 
 export const OriginalGame = () => {
 
   let [score, setScore] = useState(0);
 
   return (
-    <section className="absolute top-0 h-screen w-screen bg-gradient-to-b from-radial-gradient-from to-radial-gradient-to">
-      <section className="score-container">
-        <div>
-          <img alt="logo" src={logo} />
+    <section className="fixed top-0 left-0 h-screen w-screen bg-gradient-to-b from-radial-gradient-from to-radial-gradient-to">
+      <section className="grid grid-cols-2 w-5/6 m-auto border-4 border-header-outline rounded-lg p-3 my-10">
+        <div className="col-span-1 m-auto ml-2">
+          <img className="w-24" alt="logo" src={logo} />
         </div>
-        <div>
-          <span> SCORE </span>
-          <h2> {score} </h2>
+        <div className="col-span-1 bg-white w-24 h-20 m-auto mr-0 rounded-md shadow-lg">
+          <p className="text-score-text text-xs font-semibold mt-2"> SCORE </p>
+          <h2 className="text-dark-text font-semibold text-5xl"> {score} </h2>
         </div>
       </section>
-      <section>
-        <img alt="paper" src={paper_icon} />
-        <img alt="scissor" src={scissors_icon} />
-        <img alt="rock" src={rock_icon} />
+      <div className="w-screen absolute z-0 mt-14"> <img className="m-auto w-52" src={bgImage} alt="bg" /> </div>
+      <section className="grid grid-cols-2 z-10">
+        <div className="col-span-1 m-auto z-10 bg-gradient-to-tr rounded-full p-4 shadow-inner  from-paper-from to-paper-to"><button className="bg-white w-24 h-24 rounded-full"><img className="m-auto" alt="paper" src={paper_icon} /></button></div>
+        <div className="col-span-1 m-auto z-10 bg-gradient-to-tr rounded-full p-4 shadow-inner  from-scissors-from to-scissors-to"><button className="bg-white w-24 h-24 rounded-full"><img className="m-auto" alt="scissor" src={scissors_icon} /></button></div>
+        <div className="col-span-2 m-auto z-10 bg-gradient-to-tr rounded-full p-4 shadow-inner  from-rock-from to-rock-to my-4"><button className="bg-white w-24 h-24 rounded-full"><img className="m-auto" alt="rock" src={rock_icon} /></button></div>
       </section>
-      <div>
+      <div className="invisible fixed">
         <img alt="rules" src={rules} />
       </div>
       <button> RULES </button>
